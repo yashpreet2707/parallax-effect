ef6f155cf60564dd9759baff2e6dbcc2dd6b7965
yashpreet2707 <yashpreet1686@gmail.com>
2023-08-27 23:54:20 +0530

    updated files

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const parallax = document.getElementById("parallax") ;
 
 window.addEventListener("scroll", ()=>{
-    let offset = window.pageYOffset ; // it's not depreceated, it's just showing the warning xd
+    let offset = window.pageYOffset ; 
+    // it's not depreceated, it's just showing the warning xd
     parallax.style.backgroundPositionY = offset*0.7 + "px" ;
 })
