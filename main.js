315bdffa934e59e630b456fa0e717403e4e89497
yashpreet2707 <yashpreet1686@gmail.com>
2023-08-27 23:52:13 +0530

    parallax effect div1

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const parallax = document.getElementById("parallax") ;
+
+window.addEventListener("scroll", ()=>{
+    let offset = window.pageYOffset ; // it's not depreceated, it's just showing the warning xd
+    parallax.style.backgroundPositionY = offset*0.7 + "px" ;
+})
